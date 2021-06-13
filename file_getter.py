1774304564ead4a0535998c8a80e184f7fa890b0
Alex Rakowski <alexrakowski90@gmail.com>
2021-06-13 00:21:24 -0700

    adding file_getter.py with argparse

--- file_getter.py
+++ file_getter.py
@@ -1,10 +1,20 @@
+from typing_extensions import ParamSpecArgs
 import requests 
 from typing import Union
 import pathlib
 import argparse
 
 
+def parser():
 
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--dpc_nanotube", action='store_true')
+    parser.add_argument("--make_probe_templates", action='store_true')
+
+    args = parser.parse_args()
+
+    return args
 
 
 def download_file_from_google_drive(id_:str, 
@@ -45,4 +55,25 @@ def download_file_from_google_drive(id_:str,
 
     save_response_content(response, destination)    
 
+    return None
+
+
+def __main__():
+
+    args = parser()
+
+    if args.dpc_nanotube:
+        download_file_from_google_drive("1bHv3u61Cr-y_GkdWHrJGh1lw2VKmt3UM", "data/carbon_nanotube_data.h5")
+
+    else:
+        pass
+
+    if args.make_probe_templates:
+        download_file_from_google_drive("1iBSANFQT9eacpn7aAE6PmFdIRYQxenGg", "data/bullseye_probe_scan_edge.dm4")
+        download_file_from_google_drive("1QTcSKzZjHZd1fDimSI_q9_WsAU25NIXe", "data/vacuum_probe_20x20.dm4")
+        download_file_from_google_drive("1p9GaV0k628_afbdqFW62rHObcjQ09qyl", "data/NiPt_nanoparticle_20x20.dm3")
+        download_file_from_google_drive("1sUrPEgM1wWyTh-LJ30lGUhcXklHj6ajC", "data/twinBoundary_ShitengZhao20190115MEA.h5")
+    else: 
+        pass
+    
     return None
