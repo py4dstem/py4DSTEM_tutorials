ac146ca985289fc6e39d13b07f87e5a26684f781
alex-rakowski <alexrakowski90@gmail.com>
2021-06-14 14:01:35 -0700

    updated to file_getter.py args
    added 
    - option to download all the data 
    - data path for strain_simple_quantum_well.ipynb

--- file_getter.py
+++ file_getter.py
@@ -10,7 +10,8 @@ def parser():
 
     parser.add_argument("--dpc_nanotube", action='store_true')
     parser.add_argument("--make_probe_templates", action='store_true')
-
+    parser.add_argument("--strain_simple_quantum_well", action='store_true')
+    parser.add_argument("--all", action="store_true", help='download all the files')
     args = parser.parse_args()
 
     return args
@@ -61,18 +62,23 @@ def __main__():
 
     args = parser()
 
-    if args.dpc_nanotube:
+    if args.dpc_nanotube or args.all:
         download_file_from_google_drive("1bHv3u61Cr-y_GkdWHrJGh1lw2VKmt3UM", "/home/jovyan/data/carbon_nanotube_data.h5")
 
     else:
         pass
 
-    if args.make_probe_templates:
+    if args.make_probe_templates or args.all:
         download_file_from_google_drive("1iBSANFQT9eacpn7aAE6PmFdIRYQxenGg", "/home/jovyan/data/bullseye_probe_scan_edge.dm4")
         download_file_from_google_drive("1QTcSKzZjHZd1fDimSI_q9_WsAU25NIXe", "/home/jovyan/data/vacuum_probe_20x20.dm4")
         download_file_from_google_drive("1p9GaV0k628_afbdqFW62rHObcjQ09qyl", "/home/jovyan/data/NiPt_nanoparticle_20x20.dm3")
         download_file_from_google_drive("1sUrPEgM1wWyTh-LJ30lGUhcXklHj6ajC", "/home/jovyan/data/twinBoundary_ShitengZhao20190115MEA.h5")
     else: 
+        pass
+    
+    if args.strain_simple_quantum_well or args.all:
+        download_file_from_google_drive("1GTxIaxET98vdRMbF8IhhaahipC8NX7iP", "/home/jovyan/data/IanQW2.hspy")
+    else:
         pass
     
     return None
